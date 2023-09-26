58848ebb70446b31b9b9f56779b0c2fc056d3255
mellenena <jemiruth1@gmail.com>
2023-09-25 19:09:35 -0500

    Added getters and setters for Date attribute on Note class.

--- src/main/java/com/example/twonotes/Note.java
+++ src/main/java/com/example/twonotes/Note.java
@@ -1,11 +1,15 @@
 package com.example.twonotes;
 
+import java.util.Date;
+
 public class Note implements Organizable {
     private String name;
+    private Date date;
     private String content;
 
-    public Note(String name, String content) {
+    public Note(String name, Date date, String content) {
         this.name = name;
+        this.date = date;
         this.content = content;
     }
 
@@ -24,5 +28,11 @@ public class Note implements Organizable {
     }
     public void setContent(String content){
         this.content = content;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
     }
 }
