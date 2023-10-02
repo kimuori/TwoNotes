2dda721694821143fe085679c0c563c76f007473
mellenena <jemiruth1@gmail.com>
2023-10-02 12:48:55 -0500

    1. Added main.fxml UI scene of the main note-taking application.
    2. 'login.fxml' has added login button on action functionality.
    
    3. 'Controller.java' has been modified to add button functionality to switch scenes between login and main.

--- src/main/java/com/example/twonotes/Controller.java
+++ src/main/java/com/example/twonotes/Controller.java
@@ -1,14 +1,30 @@
 package com.example.twonotes;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+import java.util.Objects;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button loginButton;
+
+    public void loginButtonOnAction (ActionEvent event) throws IOException{
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main.fxml")));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false); //prevents window resizing
+        stage.show();
     }
 }
