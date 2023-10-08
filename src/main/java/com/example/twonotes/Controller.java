483d8af952f0e96e00051d9d50a34b9eaf74ab83
mellenena <jemiruth1@gmail.com>
2023-10-08 17:12:17 -0500

    Added signup UI .fxml. Modified button controller functionality inside the Controller.java to switch scenes between login.fxml and signup.fxml.

--- src/main/java/com/example/twonotes/Controller.java
+++ src/main/java/com/example/twonotes/Controller.java
@@ -27,4 +27,22 @@ public class Controller {
         stage.setResizable(false); //prevents window resizing
         stage.show();
     }
+
+    public void signupPageButtonOnAction (ActionEvent event) throws IOException{
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("signup.fxml")));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false); //prevents window resizing
+        stage.show();
+    }
+
+    public void loginPageButtonOnAction (ActionEvent event) throws IOException{
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login.fxml")));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false); //prevents window resizing
+        stage.show();
+    }
 }
