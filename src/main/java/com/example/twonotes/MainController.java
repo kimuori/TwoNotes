ed6be75c4b5bc306155e8255acacbcfe63df91fd
Jemina M <jemiruth1@gmail.com>
2023-11-07 10:51:47 -0600

    Modifed the MainController and added javadoc on methods.

--- src/main/java/com/example/twonotes/MainController.java
+++ src/main/java/com/example/twonotes/MainController.java
@@ -27,19 +27,23 @@ public class MainController implements Initializable {
     @FXML
     private TextField fileTitle;
 
-    //private int noteCounter = 0;
-
+    /**
+     * addNoteButtonOnAction method is called when "Add Note" button is clicked. It will
+     * create a test file and the user can choose its name. The note content
+     * will show up in the local UI.
+     *
+     * @author Jemina, Madeline, Jet
+     * @param event
+     */
     @FXML
-    // Method is called when "Add Note" button is clicked
     public void addNoteButtonOnAction(ActionEvent event) {
-        //noteCounter++;
         // FileChooser used to open a file dialog for the user to specify a file name & location within the directory
         FileChooser fileChooser = new FileChooser();
         // Extension filter is added to restrict the selection to txt files only
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
         File selectedFile = fileChooser.showSaveDialog(stage); // Open a Save dialog
-
-        if (selectedFile != null) { // Checks if a file was selected
+        // Checks if a file was selected; it will write a default string into the text file.
+        if (selectedFile != null) {
             try {
                 String content = "Write content here!.";
                 writeTextToFile(selectedFile, content); //method to write content on the text file
@@ -47,9 +51,10 @@ public class MainController implements Initializable {
                 e.printStackTrace(); // Stack trace is printed to the console if error occurs during file creation or writing process
             }
         }
+        // NOTE: Added functionality based on open_File method to show the created file to the local UI.
         try  {
-            Scanner scan = new Scanner(selectedFile);
-            Scanner scanTitle = new Scanner(selectedFile.getName());
+            Scanner scan = new Scanner(selectedFile); // scans the file that has been created above
+            Scanner scanTitle = new Scanner(selectedFile.getName()); //scans the file's name
 
             Note aNote = new Note(scanTitle.toString()); //stores the new note instantiation to the Note object
 
@@ -69,16 +74,28 @@ public class MainController implements Initializable {
 
     }
 
-    // writeTextToFile method takes in params file & content
-    // and uses the BufferedWriter class to efficiently write the content to the file
-    // and also ensure the file is closed correctly
+    /**
+     * writeTextToFile method takes in params file & content.
+     * It uses the BufferedWriter class to write the content to the file
+     * and ensures the file is closed correctly.
+     *
+     * @author Madeline
+     * @param file, content
+     */
     private void writeTextToFile(File file, String content) throws IOException {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
             writer.write(content);
         }
     }
 
-    // Implemented by Jet Lao
+    /**
+     * open_File method is called when "Open a File" is clicked from the upper left corner.
+     * This gives the user to traverse through the directory and select a text file.
+     * The textfile will show up in the local UI.
+     *
+     * @author Jet
+     * @param actionEvent
+     */
     @FXML
     public void open_File(ActionEvent actionEvent) {
         File file = filechooser.showOpenDialog(new Stage());
@@ -103,6 +120,18 @@ public class MainController implements Initializable {
 
 
     }
+
+    /**
+     *
+     * @author Jet
+     * @param location
+     * The location used to resolve relative paths for the root object, or
+     * {@code null} if the location is not known.
+     *
+     * @param resources
+     * The resources used to localize the root object, or {@code null} if
+     * the root object was not localized.
+     */
     @Override
     public void initialize (URL location, ResourceBundle resources) {
         filechooser.setInitialDirectory(new File("C:\\Users"));
