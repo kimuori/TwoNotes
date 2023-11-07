f34206aa2938249432315bd486f490a24542fc37
Laoj25 <laojet25@gmail.com>
2023-11-06 19:33:27 -0600

    New Note instance variables/getters/setters deleted Date as a variable added FileController class edited main.fxml file for menuItem

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module com.example.twonotes {
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
 
     opens com.example.twonotes to javafx.fxml;
     exports com.example.twonotes;
